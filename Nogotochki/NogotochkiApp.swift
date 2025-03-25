c5ce2967da3d75c5cc3c1c083d4030b297e85f07
emaksimova <maksimovaswift@gmail.com>
2025-03-25 16:46:27 +0300

    Initial Commit

---
+++ Nogotochki/NogotochkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NogotochkiApp.swift
+//  Nogotochki
+//
+//  Created by Евгения Максимова on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NogotochkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
